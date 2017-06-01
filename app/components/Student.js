dc0f8e21a0baa74ddd47f016f793b67353215514
CindySchalit <cindyschalit@gmail.com>
2017-06-01 11:41:09 -0700

    can navigate to view student's campus from student page

--- app/components/Student.js
+++ app/components/Student.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default function Student(props) {
   const name = props.selectedStudent.name;
   const email = props.selectedStudent.email;
-  const campus = props.selectedStudent.campus && props.selectedStudent.campus.name;
+  const campusName = props.selectedStudent.campus && props.selectedStudent.campus.name;
+  const campusId = props.selectedStudent.campusId;
 
   return (
     <div>
       <h3>This is { name }'s Page.</h3>
-      <p>You can reach { name } at { email }.</p>
-      <p>{ name } is a student at { campus }.</p>
+      <p>You can reach { name } at { email }.  { name } is a student at { campusName } Campus.</p>
+      <p>For more information on { campusName } Campus, please click here.
+          <Link className="thumbnail" to={`campuses/${campusId}`}>More Info</Link>
+      </p>
+
     </div>
   )
 }
