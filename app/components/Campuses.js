bcfc23dc5c7d845a0dcd5954e47f64315a0e8d6c
CindySchalit <cindyschalit@gmail.com>
2017-05-31 21:36:26 -0700

    campuses page rendering with all campuses

--- app/components/Campuses.js
+++ app/components/Campuses.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default function Campuses(props) {
   console.log('PROPS IN CAMPUSES: ', props);
+  const campuses = props.campuses;
 
   return (
     <div>
-      <h3>Campuses</h3>
+      <h2>Campuses</h2>
+      <div className="row">
+      {
+        campuses && campuses.map(c => (
+          <div className="col-xs-4" key={ c.id }>
+            <Link className="thumbnail" to={`campuses/${c.id}`}>
+              <img src={ c.image } />
+              <div className="caption">
+                <h3>
+                  <span>{ c.name }</span>
+                </h3>
+              </div>
+            </Link>
+          </div>
+        ))
+      }
+      </div>
     </div>
   );
 }
